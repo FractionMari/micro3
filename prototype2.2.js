95ad260c912e1089f019672b6a24ebf1ecc0a0e6
fractionMari <lesteberg@gmail.com>
2021-10-29 11:36:36 +0200

    Trying to improve the second prototype

--- prototype2.2.js
+++ prototype2.2.js
@@ -825,63 +825,13 @@ function updateFieldIfNotNull(fieldName, value, precision=2){
 }
           else{
 
-            function myFunction() {
+/*             function myFunction() {
               pitchChange.pitch = 0;
-            }
-
-    createRandomness();
-    document.getElementById("timeSign").innerHTML =
-    "Time signature: " + "<br>" + random0 + " / 16";
-  
-    document.getElementById("tempo").innerHTML =
-    "BPM: " + "<br>" + Tone.Transport.bpm.value;
+            } */
 
 
-
-    const seq0 = new Tone.Sequence((time, note) => {
-      synth0.triggerAttackRelease(note, 2, time);
-      // subdivisions are given as subarrays
-  }, randomArray).start(0);
-  seq0.playbackRate = 0.5;
-  
-  const seq = new Tone.Sequence((time, note) => {
-      synth.triggerAttackRelease(note, 2, time);
-      // subdivisions are given as subarrays
-  }, randomArray).start(0);
-  seq.playbackRate = 0.5;
-  
-  const seq2 = new Tone.Sequence((time, note) => {
-     synth2.triggerAttackRelease(note, 0.8, time);
-     // subdivisions are given as subarrays
-  }, randomArray2).start(0);
-  
-  const seq3 = new Tone.Sequence((time, note) => {
-     synth3.triggerAttackRelease(note, 0.8, time);
-     // subdivisions are given as subarrays
-  }, randomArray3).start(0);
-  
-  const seq4 = new Tone.Sequence((time, note) => {
-  synth4.triggerAttackRelease(note, 0.3, time);
-  // subdivisions are given as subarrays
-  }, randomMelodyArray).start(0);
-  seq4.playbackRate = 0.5;
-  
-  const pattern6 = new Tone.Sequence(function(time, note){
-  synth6.triggerAttackRelease(note, 0.9);
-  }, randomHiHatArray).start();
-  pattern6.playbackRate = 0.5;
-  
-  const pattern5 = new Tone.Sequence(function(time, note){
-  synth5.triggerAttackRelease(note, 0.9);
-  }, randomDrumArray).start();
-  pattern5.playbackRate = 0.5;
-
-
-    Tone.Transport.start();
-    Tone.start();
-
-    pitchChange.pitch = pitchChangePitch,
-     setTimeout(myFunction, 2000);
+    pitchChange.pitch = pitchChangePitch;
+    //  setTimeout(myFunction, 2000);
        
 
 
